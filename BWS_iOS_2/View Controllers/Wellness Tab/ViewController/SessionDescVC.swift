b78c8d07b94497b66d93698fd937e0ac5f65a79e
sapna kardas <>
2021-07-20 09:39:38 +0530

    design xd (86-88)

---
+++ BWS_iOS_2/View Controllers/Wellness Tab/ViewController/SessionDescVC.swift
@@ -0,0 +1,40 @@
+//
+//  SessionDescVC.swift
+//  BWS_iOS_2
+//
+//  Created by Mac Mini on 20/07/21.
+//  Copyright © 2021 Dhruvit. All rights reserved.
+//
+
+import UIKit
+
+class SessionDescVC: BaseViewController {
+    
+    //MARK:- UIOutlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var lblDesc: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    //MARK:- Variables
+    
+    //MARK:- View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK:- Functions
+    override func setupUI() {
+        
+    }
+    
+    override func setupData() {
+        
+    }
+    
+    //MARK:- IBAction Methods
+    @IBAction func onTappedContinue(_ sender: UIButton) {
+    }
+    
+}
+
